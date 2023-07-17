845f817e250bccb89219fe4eb00047cd116028b3
Mieraisan <mieraisan@gmail.com>
2023-07-17 09:15:12 +0100

    Enable scolling while typing

--- TypingPall/Utils/String+Utils.swift
+++ TypingPall/Utils/String+Utils.swift
@@ -9,8 +9,9 @@ import Foundation
 
 extension String {
     func extractMismatchedRange(comparedTo placeholder: String) -> NSRange? {
+        guard !isEmpty else { return nil }
         let commonPrefix = self.commonPrefix(with: placeholder, options: .literal)
-        guard commonPrefix.count < self.count else { return nil }
+        guard commonPrefix.count < self.count, commonPrefix.count >= 1 else { return nil }
         return NSRange(location: commonPrefix.count, length: self.count - commonPrefix.count)
     }
 }
--- TypingPall/Views/TypingEditor.swift
+++ TypingPall/Views/TypingEditor.swift
@@ -12,8 +12,17 @@ struct TypingEditor: NSViewRepresentable {
     @Binding var text: String
     @Binding var placeholder: String
 
-    let placeholderTextView = NSTextView()
-    let typingTextView = NSTextView()
+    let placeholderTextView: NSTextView = {
+        let scrollView = NSTextView.scrollableTextView()
+        let textView = scrollView.documentView as! NSTextView
+        return textView
+    }()
+
+    let typingTextView: NSTextView = {
+        let scrollView = NSTextView.scrollableTextView()
+        let textView = scrollView.documentView as! NSTextView
+        return textView
+    }()
 
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -39,21 +48,24 @@ struct TypingEditor: NSViewRepresentable {
         typingTextView.font = NSFont.monospacedSystemFont(ofSize: 25, weight: .bold)
         typingTextView.textContainerInset = NSMakeSize(0, containerView.frame.size.height / 2)
 
-        containerView.addSubview(placeholderTextView)
-        containerView.addSubview(typingTextView)
+        guard let placeholderTextScrollView = placeholderTextView.superview?.superview,
+              let typingTextScrollView = typingTextView.superview?.superview else { return containerView}
 
-        placeholderTextView.translatesAutoresizingMaskIntoConstraints = false
-        typingTextView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(placeholderTextScrollView)
+        containerView.addSubview(typingTextScrollView)
+
+        placeholderTextScrollView.translatesAutoresizingMaskIntoConstraints = false
+        typingTextScrollView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            placeholderTextView.topAnchor.constraint(equalTo: containerView.topAnchor),
-            placeholderTextView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-            placeholderTextView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            placeholderTextView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
-            typingTextView.topAnchor.constraint(equalTo: containerView.topAnchor),
-            typingTextView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-            typingTextView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            typingTextView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            placeholderTextScrollView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            placeholderTextScrollView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            placeholderTextScrollView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            placeholderTextScrollView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            typingTextScrollView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            typingTextScrollView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            typingTextScrollView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            typingTextScrollView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
         ])
 
         return containerView
@@ -78,6 +90,13 @@ class Coordinator: NSObject, NSTextViewDelegate {
     func textDidChange(_ notification: Notification) {
         guard let textView = notification.object as? NSTextView, textView === parent.typingTextView else { return }
 
+        defer {
+            if let cursorPosition = cursorPosition(in: textView),
+                let scrollView = textView.superview?.superview as? NSScrollView {
+                scrollView.scroll(cursorPosition)
+            }
+        }
+
         defer { parent.text = textView.string }
 
         changeTypingTextColorIfNeeded(textView, placeholder: parent.placeholder)
@@ -87,12 +106,25 @@ class Coordinator: NSObject, NSTextViewDelegate {
         guard !textView.string.isEmpty else { return }
 
         guard let range = textView.string.extractMismatchedRange(comparedTo: placeholder) else {
-            textView.setTextColor(.systemGreen, range: NSMakeRange(0, textView.string.count - 1))
+            textView.setTextColor(.systemGreen, range: NSMakeRange(0, textView.string.count))
             return
         }
 
-        textView.setTextColor(.red, range: NSMakeRange(range.location, textView.string.count - 1))
-        textView.setTextColor(.systemGreen, range: NSMakeRange(0, range.location - 1))
+        textView.setTextColor(.red, range: range)
+        textView.setTextColor(.systemGreen, range: NSMakeRange(0,  range.location - 1))
+    }
+
+    func cursorPosition(in textView: NSTextView) -> NSPoint? {
+        guard let textContainer = textView.textContainer,
+              let layoutManager = textView.layoutManager else { return nil }
+
+        let glyphRange = layoutManager.glyphRange(forCharacterRange: textView.selectedRange(), actualCharacterRange: nil)
+        var rect = layoutManager.boundingRect(forGlyphRange: glyphRange, in: textContainer)
+
+        rect.origin.x += textContainer.lineFragmentPadding
+        rect.origin.y += textView.textContainerInset.height
+
+        return rect.origin
     }
 }
 
