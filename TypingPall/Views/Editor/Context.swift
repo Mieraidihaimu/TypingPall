ee6da96ddb4bc5480cf2f05d7a742033fcab9e06
Ben <bcylin@users.noreply.github.com>
2023-08-12 16:07:32 +0100

    Modify text colour when textDidChange(_:)

--- TypingPall/Views/Editor/Context.swift
+++ TypingPall/Views/Editor/Context.swift
@@ -3,6 +3,9 @@ import AppKit
 final class Coordinator: NSObject, NSTextViewDelegate {
     var parent: TypingEditor
 
+    private var textView: NSTextView { parent.typingTextView }
+    private var placeholderTextView: NSTextView { parent.placeholderTextView }
+
     init(_ parent: TypingEditor) {
         self.parent = parent
     }
@@ -17,6 +20,8 @@ final class Coordinator: NSObject, NSTextViewDelegate {
            !scrollView.visibleRect.contains(cursorPosition) {
             scrollView.scroll(cursorPosition)
         }
+
+        changeTextColorIfNeeded()
     }
 
     private func cursorPosition(in textView: NSTextView) -> NSPoint? {
@@ -30,5 +35,22 @@ final class Coordinator: NSObject, NSTextViewDelegate {
         rect.origin.y += textView.textContainerInset.height
 
         return rect.origin
+    }
+
+    func changeTextColorIfNeeded() {
+        guard !textView.string.isEmpty else { return }
+
+        let numberOfTypedCharacters = textView.string.count
+
+        guard let mismatchedRange = textView.string.extractMismatchedRange(comparedTo: placeholderTextView.string) else {
+            textView.setTextColor(.systemGreen, range: NSMakeRange(0, numberOfTypedCharacters))
+            return
+        }
+
+        if mismatchedRange.location > 1 {
+            textView.setTextColor(.systemGreen, range: NSMakeRange(0,  mismatchedRange.location - 1))
+        }
+
+        textView.setTextColor(.red, range: mismatchedRange)
     }
 }
